fa37b8df79df697d3b2084f7d14ed2ec659ae12d
Awais Mubeen <awaismubeen@gmail.com>
2025-03-15 12:53:48 +0500

    Update Package.swift
    target added

--- Package.swift
+++ Package.swift
@@ -3,22 +3,41 @@
 
 import PackageDescription
 
+//let package = Package(
+//    name: "DeepSwiftSeek",
+//    products: [
+//        // Products define the executables and libraries a package produces, making them visible to other packages.
+//        .library(
+//            name: "DeepSwiftSeek",
+//            targets: ["DeepSwiftSeek"]),
+//    ],
+//    targets: [
+//        // Targets are the basic building blocks of a package, defining a module or a test suite.
+//        // Targets can depend on other targets in this package and products from dependencies.
+//        .target(
+//            name: "DeepSwiftSeek"),
+//        .testTarget(
+//            name: "DeepSwiftSeekTests",
+//            dependencies: ["DeepSwiftSeek"]
+//        ),
+//    ]
+//)
+
+// swift-tools-version: 5.5
+import PackageDescription
+
 let package = Package(
-    name: "DeepSwiftSeek",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "DeepSwiftSeek",
-            targets: ["DeepSwiftSeek"]),
+    name: "YourPackage",
+    platforms: [
+        .macOS(.v12) // ✅ Set macOS 12 or later
+    ],
+    dependencies: [
+        .package(url: "https://github.com/awaismubeen/DeepSwiftSeek", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DeepSwiftSeek"),
-        .testTarget(
-            name: "DeepSwiftSeekTests",
-            dependencies: ["DeepSwiftSeek"]
-        ),
+            name: "YourTarget",
+            dependencies: []
+        )
     ]
 )
