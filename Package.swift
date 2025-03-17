deeb81be2b55c5273b5f3f61f3d3f73d0772a7f9
Awais Mubeen <awaismubeen@gmail.com>
2025-03-17 11:47:56 +0500

    Update Package.swift
    bug fixed

--- Package.swift
+++ Package.swift
@@ -5,20 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "DeepSwiftSeek",
+    platforms: [
+        .macOS(.v12) // ✅ Set macOS 12 or later
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "DeepSwiftSeek",
-            targets: ["DeepSwiftSeek"]),
-    ],
-    platforms: [
-        .macOS(.v12) // ✅ Set macOS 12 or later
+            targets: ["DeepSwiftSeek"]
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "DeepSwiftSeek"),
+            name: "DeepSwiftSeek"
+        ),
         .testTarget(
             name: "DeepSwiftSeekTests",
             dependencies: ["DeepSwiftSeek"]
